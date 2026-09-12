7eea90f24f751596188b468607ed20244aeef427
ArthurCallaghan <pau.escobedo04@gmail.com>
2026-09-12 16:52:18 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -3,10 +3,9 @@
 self.addEventListener("install", () => self.skipWaiting());
 
 self.addEventListener("activate", (event) => {
-  event.waitUntil(caches.keys().then((names) => Promise.all(names.map((name) => caches.delete(name)))).then(() => self.clients.claim()));
-});
-
-self.addEventListener("fetch", (event) => {
-  if (event.request.method !== "GET" || new URL(event.request.url).origin !== self.location.origin) return;
-  event.respondWith(fetch(new Request(event.request, { cache: "no-store" })));
+  event.waitUntil(
+    caches.keys()
+      .then((names) => Promise.all(names.map((name) => caches.delete(name))))
+      .then(() => self.registration.unregister())
+  );
 });
